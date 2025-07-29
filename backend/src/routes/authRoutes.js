ebedaafda3a3c352874685d9cdaa94d57e05061f
Edrick Manoel <edrick_42@icloud.com>
2025-07-28 21:35:39 -0300

    creating Docker files and configurations

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import prisma from '../prismaClient.js'
 
 const router = express.Router()
 
 // Register a new user endpoint /auth/register
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     // destructing the body into two variables {username and password}
     const {username, password} = req.body
 
@@ -14,20 +15,49 @@ router.post('/register', (req, res) => {
 
     // saving the new user
     try {
-        
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                password: hashedPassword
+            }
+        })
+
+        res.status(201).json(newUser)
     } catch (error) {
         console.log(log)
         res.sendStatus(503)
     }
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     // destructing the body into two variables {username and password}
     const {username, password} = req.body
     
     // verifing database here
     try {
-        
+        const user = await prisma.user.findUnique({
+            where: {
+                username
+            }
+        })
+
+        // if user not found
+        if (!user) {
+            return res.status(404).json({message: 'User not found'})
+        }
+
+        // check password
+        const isPasswordValid = bcrypt.compareSync(password, user.password)
+
+        // if password is invalid
+        if (!isPasswordValid) {
+            return res.status(401).json({message: 'Invalid password'})
+        }
+
+        // create a token
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '1h'})
+
+        res.status(200).json({token})
     } catch (error) {
         console.log(log)
         res.sendStatus(503)
